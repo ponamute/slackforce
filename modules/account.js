ea2d9379bac5f6c1e5590ee81dc3fa00e4bc31e3
Daren Nicholson <daren@genomemedical.com>
2017-06-30 10:46:19 -0700

    removed type

--- modules/account.js
+++ modules/account.js
@@ -14,7 +14,7 @@ exports.execute = (req, res) => {
 
     let slackUserId = req.body.user_id,
         oauthObj = auth.getOAuthObject(slackUserId),
-        q = "SELECT Id, Name, OwnerID, Website, DNR__c, DNR_Reason__c, RecordType FROM Account WHERE Name LIKE '%" + req.body.text + "%' LIMIT 5";
+        q = "SELECT Id, Name, OwnerID, Website, DNR__c, DNR_Reason__c, RecordTypeId FROM Account WHERE Name LIKE '%" + req.body.text + "%' LIMIT 5";
 
     force.query(oauthObj, q)
         .then(data => {
@@ -24,9 +24,6 @@ exports.execute = (req, res) => {
                 accounts.forEach(function(account) {
                     let fields = [];
                     fields.push({title: "Name", value: account.Name, short:true});
-                    if (account.RecordType) {
-                      fields.push({title: "Type", value: account.RecordType});
-                    }
                     if (account.Website) {
                         fields.push({title: "Website", value: account.Website, short:true});
                     }
